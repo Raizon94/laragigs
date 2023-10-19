7d86754b3490e70a055bd481c3f0410a7ed2a4fb
Raizon94 <williamatef94@gmail.com>
2023-10-19 19:36:20 +0200

    3:13

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,11 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 //Store route
 Route::post('/listings', [ListingController::class, 'store']);
 
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+Route::put('/listings/{listing}', [ListingController::class,'update']);
+
+Route::delete('/listings/{listing}', [ListingController::class,'destroy']);
+
 //Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
