f234428a1d585a51cb5b6f0d638528b27b962fb4
Raizon94 <williamatef94@gmail.com>
2023-10-21 18:21:38 +0200

    3:46

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,12 @@ Route::get('/register', [UserController::class, 'create']);
 Route::post('/users', [UserController::class, 'store']);
 
 
+Route::post('/logout', [UserController::class,'logout']);
+
+Route::get('/login', [UserController::class,'login']);
+
+Route::post('/users/authenticate', [UserController::class,'authenticate']);
+
 
 //Common resource routes:
 /*  index: show all listings
